3b6f1b72b84b8ccccbee77396b77791eeac40c2e
Sen Demina <senjademina@gmail.com>
2023-09-09 17:21:19 -0400

    added bow+arrow

--- Assets/Scripts/Aim.cs
+++ Assets/Scripts/Aim.cs
@@ -6,15 +6,18 @@ public class Aim : MonoBehaviour
 {
     [SerializeField] private GameObject bow;
 
+    //void Update()
+    //{
+    //    Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //    bow.transform.LookAt(target);
+    //}
+
     void Update()
     {
-        Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        bow.transform.LookAt(target);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f))
+        {
+            bow.transform.LookAt(raycastHit.point);
+        }
     }
-
-    //void Method2()
-    //{
-    //    RaycastHit rayHit;
-    //    if(Physics.Raycast())
-    //}
 }
